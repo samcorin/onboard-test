0fd4303ec1a360d7cff4256f6b8e23dae29126af
Sam Corin <samcorin@gmail.com>
2017-04-08 15:41:23 +0900

    Prepare for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'managers#index'
   resources :managers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
