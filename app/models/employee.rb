7e09db65e233ed177b33587d397601d6128b2767
Sam Corin <samcorin@gmail.com>
2017-04-09 20:38:31 +0900

    add styling

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,14 @@
 class Employee < ApplicationRecord
   # belongs_to :manager
   belongs_to :user
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+
+  after_create :send_admin_mail
+
+  def send_admin_mail
+    # UserMailer.send_new_user_message(self).deliver
+  end
 end
