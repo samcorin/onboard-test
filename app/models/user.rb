d94c34582c7fa24a6424165d41bb5d951cbb5bb2
Sam Corin <samcorin@gmail.com>
2017-04-10 08:56:54 +0900

    fixed create user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :employees, dependent: :destroy
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
   validates :email, presence: true
 end
